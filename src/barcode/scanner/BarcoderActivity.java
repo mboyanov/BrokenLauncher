085fa2dff6f74c512ce3ebf604f933ef2721ac2d
mboyanov <mboyanov@gmail.com>
2014-06-01 13:23:00 +0100

    We can now scan contacts

--- src/barcode/scanner/BarcoderActivity.java
+++ src/barcode/scanner/BarcoderActivity.java
@@ -1,36 +1,17 @@
 package barcode.scanner;
 
-import java.io.BufferedReader;
-
-import android.net.Uri;
-import android.os.Vibrator;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.graphics.Color;
-import android.media.AudioManager;
+import android.database.Cursor;
 import android.media.MediaPlayer;
-import android.os.AsyncTask;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.BarcodeTest.IntentIntegrator;
@@ -45,7 +26,8 @@ public class BarcoderActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		IntentIntegrator.initiateScan(this);
+		this.extractContacts();
+		//IntentIntegrator.initiateScan(this);
 		
 	}
 
@@ -73,15 +55,7 @@ public class BarcoderActivity extends Activity {
 		}
 	}
 
-	private String getEvent() {
-		SharedPreferences prefs = this.getSharedPreferences("myPrefsKey",
-				Context.MODE_PRIVATE);
-		return Integer.toString(prefs.getInt("eventid", -1));
-	}
-
-	public void show(int i) {
-
-	}
+	
 
 	public void destroy(View v) {
 
@@ -89,10 +63,35 @@ public class BarcoderActivity extends Activity {
 	}
 
 	
-	public void mainmenu(View v) {
-		onCreate(null);
-	}
-
 	
 
+	private void extractContacts(){
+	
+		ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                  String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                  String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                  if (Integer.parseInt(cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                     Cursor pCur = cr.query(
+                               ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                               null,
+                               ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                               new String[]{id}, null);
+                     while (pCur.moveToNext()) {
+                         String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                         Toast.makeText(this, "Name: " + name + ", Phone No: " + phoneNo, Toast.LENGTH_SHORT).show();
+                     }
+                    pCur.close();
+                }
+            }
+        }
+	
+	}
 }
+
+
+
